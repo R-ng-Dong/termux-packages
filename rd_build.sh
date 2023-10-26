79877f928269b1eef1873bf75625db92552f4b5f
nguyenvietanh15081997 <nguyenvietanh15081997@gmail.com>
2023-10-26 17:02:02 +0700

    add build wget

--- rd_build.sh
+++ rd_build.sh
@@ -4,6 +4,7 @@ git submodule update --init --recursive
 ./build-package.sh -a arm lvgl
 ./build-package.sh -a arm openssh
 ./build-package.sh -a arm vim
+./build-package.sh -a arm wget
 ./build-package.sh -a arm libmosquitto
 ./build-package.sh -a arm libsqlite
 ./build-package.sh -a arm htop
