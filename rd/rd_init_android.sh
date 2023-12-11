411e190acfb52583ac6742d8f947dd0b693fbbd1
thinpv <thindcna@gmail.com>
2023-12-11 09:50:06 +0700

    move data to vendor

--- rd/rd_init_android.sh
+++ rd/rd_init_android.sh
@@ -1,44 +1,22 @@
 #!/system/bin/sh
+
 tar -xzf /data/rd/rd.tar.gz -C /data/rd
 mount -o rw,remount /system
-#yes | cp system/* /system/ -rfi
+mount -o rw,remount /vendor
+
+chmod +x system/bin/*
+chmod 644 etc/init/rd.rc
+
 rm /system/bin/demo_s8
 rm /system/bin/sshd
-cp system/* /system/ -r
-
-cp /data/rd/rd.rc /etc/init
-chmod 644 /etc/init/rd.rc
-
-#cp /data/rd/lvgl.sh /system/bin
-#chmod +x /system/bin/lvgl.sh
-
-cp /data/rd/smh.sh /system/bin
-chmod +x /system/bin/smh.sh
-
-cp /data/rd/mosquitto.sh /system/bin
-chmod +x /system/bin/mosquitto.sh
-
-mkdir /etc/mosquitto/
-cp /data/rd/mosquitto.conf /etc/mosquitto/
-
-cp /data/rd/sshremote.sh /system/bin
-chmod +x /system/bin/sshremote.sh
-
-cp /data/rd/rd_init.sh /system/bin
-chmod +x /system/bin/rd_init.sh
-
-#ln -s /system/bin/vim /system/bin/vi
-
 #rm /system/priv-app/Launcher3/ -rf
 
-ln -s /system/bin/sh /system/bin/bash
+cp system/* /system/ -r
+cp vendor/* /vendor/ -r
+cp etc/* /etc/ -r
+
 ssh-keygen -A
 
-mount -o rw,remount /vendor
-mkdir /vendor/data
-mkdir /vendor/data/rd
-cp /data/rd/home /vendor/data/rd
-
-rm /data/rd -rf
+# rm /data/rd -rf
 
 # reboot
