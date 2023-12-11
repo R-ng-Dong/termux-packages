976bff9b4aa3be722ff3c4817611604f03ef62f0
thinpv <thindcna@gmail.com>
2023-12-11 16:08:10 +0700

    change timezone;add HcApp, FastScan apk

--- rd/system/bin/rd_init.sh
+++ rd/system/bin/rd_init.sh
@@ -1,8 +1,17 @@
 #!/system/bin/sh
 
 if [[ `ls /data/rd/home/.termux_authinfo 2> /dev/null` ]]; then
-	echo "File exists";
+	echo "RAL start";
 else
-	echo "File doesn't exist";
+	echo "RAL init config";
 	cp /vendor/data/* /data/ -r
+	wm overscan 0,0,-96,0
+	service call alarm 3 s16 Asia/Ho_Chi_Minh
+	content insert --uri content://settings/system --bind name:s:system_locales --bind value:s:vi-VN
+	stop
+	start
 fi
+
+service call bluetooth_manager 6
+pm grant vn.com.rangdong.fastscan android.permission.ACCESS_FINE_LOCATION
+am start-foreground-service vn.com.rangdong.fastscan/.Service.MyBleService
--- rd_push.sh
+++ rd_push.sh
@@ -4,4 +4,4 @@ adb root
 adb shell su -c mkdir /data/rd
 adb push rd.tar.gz /data/rd
 adb push rd/* /data/rd
-# adb shell su -c /data/rd/rd_init_android.sh
+# adb shell su -c /data/rd/rd_install.sh
